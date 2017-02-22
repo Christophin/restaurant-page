0607cda8503f8ab94a9dda44610fa528fc1e623c
Chris Coralli <ccoralli@gmail.com>
2017-02-22 16:33:50 -0500

    cobbling together flickr api requests.

--- src/js/flickr-api.js
+++ src/js/flickr-api.js
@@ -1,17 +1,46 @@
 import $ from 'jquery';
 import {FL_KEY} from './flickr-token';
 
-function grabBgImg ()  {
+function searchFlickr ()  {
     return $.ajax({
-        url: `https://api.flickr.com/services/rest/`,
+        url: 'https://api.flickr.com/services/rest/',
         data: {
-            method: `flickr.photos.getSizes`,
+            method: 'flickr.photos.search',
             api_key: `${FL_KEY}`,
-            format: `json`,
+            format: 'json',
             nojsoncallback: 1,
-            photo_id: 13488563063,
+            tags: 'german beer',
         },
     });
 }
 
-export {grabBgImg};
+var imgIds = [];
+
+function processFlickr(data)    {
+    data.photos.photo.forEach(function(image) {
+        imgIds.push(image.id);
+    });
+}
+
+imgIds.forEach(function(image)  {
+    return $.ajax({
+        url: 'http://api.flickr.com/services/rest/',
+        data:   {
+            method: 'flickr.photos.getSizes',
+            api_key: `${FL_KEY}`,
+            format: 'json',
+            nojsoncallback: 1,
+            photo_id: `${image}`
+        }
+    });
+});
+
+
+export {searchFlickr, processFlickr};
+
+
+
+// ---------------BACKGROUND IMAGE-------------
+// mobile?: https://www.flickr.com/photos/brianeden/13488563063/sizes/z/
+// large: https://farm8.staticflickr.com/7309/13488563063_3a6dc48af3_b.jpg
+// full size: https://farm8.staticflickr.com/7309/13488563063_9cab01ca1c_h.jpg
