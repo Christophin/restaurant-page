5d71cc7b93873cb0b84e2c8c5a8b46136551f9cf
Chris Coralli <ccoralli@gmail.com>
2017-02-22 21:03:11 -0500

    added comments.

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,13 @@ import $ from 'jquery';
 import {searchFlickr} from './flickr-api';
 import { grabMenu, grabSpecial, grabNews } from './tiy-api';
 
+// api requests
 searchFlickr();
-
 grabMenu();
-
 grabSpecial();
-
 grabNews();
 
+// creates our js mobile breakpoint
 $(window).resize(function breakPoint ()  {
     if (window.innerWidth <= 480)   {
         //$('.container').html(`<div class='imgBox'><img src='https://farm8.staticflickr.com/7309/13488563063_3a6dc48af3_b.jpg' /></div>`);
--- src/js/tiy-api.js
+++ src/js/tiy-api.js
@@ -1,24 +1,28 @@
 import $ from 'jquery';
 
+// grabs our menu data
 function grabMenu () {
-  return $.ajax({
-  url: "https://json-data.herokuapp.com/restaurant/menu/3",
-  dataType: "json",
-});
+    return $.ajax({
+        url: 'https://json-data.herokuapp.com/restaurant/menu/3',
+        dataType: 'json',
+    });
 }
 
+// grabs our specials data
 function grabSpecial () {
-  return $.ajax({
-  url: "https://json-data.herokuapp.com/restaurant/special/1",
-  dataType: "json",
-});
+    return $.ajax({
+        url: 'https://json-data.herokuapp.com/restaurant/special/1',
+        dataType: 'json',
+    });
 }
 
+
+// grabs our news data
 function grabNews () {
-  return $.ajax({
-  url: "https://json-data.herokuapp.com/restaurant/news/1",
-  dataType: "json",
-});
+    return $.ajax({
+        url: 'https://json-data.herokuapp.com/restaurant/news/1',
+        dataType: 'json',
+    });
 }
 
 export  { grabMenu, grabSpecial, grabNews };
