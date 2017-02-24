81cb320467c0ce51c2b41b38e649e90d6f96e74e
Chris Coralli <ccoralli@gmail.com>
2017-02-24 13:08:01 -0500

    wired desktop template to breakpoint.

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,7 @@ import {accordion} from './accordion-function';
 
 // creates our js mobile breakpoint
 function breakPoint ()  {
-    if (window.innerWidth <= 480)   {
+    if (window.innerWidth <= 650)   {
         $('.container').html(mobileAnchors());
     } else {
         $('.container').html(desktopAnchors());
@@ -21,7 +21,13 @@ accordion();
 
 // api requests
 searchFlickr();
-grabMenu().then(mobileMenu);
+grabMenu().then(function(data)  {
+    if (window.innerWidth <= 650)   {
+        mobileMenu(data);
+    } else {
+        desktopMenu(data);
+    }
+});
 grabSpecial().then(mobileSpecial);
 grabNews().then(mobileNews);
 buildMap();
