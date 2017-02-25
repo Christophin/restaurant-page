fc73197d9daef037bb631b4adf5d22eba17c801f
Chris Coralli <ccoralli@gmail.com>
2017-02-25 14:55:11 -0500

    stemmed eye bleed.

--- src/js/templates.js
+++ src/js/templates.js
@@ -50,12 +50,25 @@ function mobileMenu (data){
                     <p class='entree-desc'>
                             ${entrees.description}
                     </p>
+                    <span class='entree-allergies'>
+                        ${entrees.allergies}
+                    </span>
+                    <span class='entree-fav'>
+                        ${entrees.favorite}
+                    </span>
+                    <span class='entree-spicy'>
+                        ${entrees.spicy}
+                    </span>
+                    <span class='entree-vegan'>
+                        ${entrees.vegan}
+                    </span>
                     <div>
                         <div class='allergy'><img src='./images/allergy-warning.png' class='icon'></div>
                         <div class='favorites'><img src='./images/favorites.png' class='icon'></div>
                         <div class='spicy>'<img src='./images/spicy.png' class='icon'></div>
                         <div class='vegan'><img src='./images/vegan.png' class='icon'></div>
                     </div>
+
         `);
     });
     //pulls GAMES information from MENU api
@@ -96,89 +109,73 @@ function desktopMenu (data){
     //pulls BEER information from MENU api
     data.Beer.forEach(function(beer){
         $('.beers').append(`
-
-                    <h3 class='beer-name is-left'>
-                    ${beer.item}
-                        <span class='beer-price is-right'>
-                            $${beer.price}
-                        </span>
-                    </h3>
-                    <h4 class='beer-style'>
+                <div class='beer'>
+                    <div class='beer-name is-left'>
+                        ${beer.item}
+                    </div>
+                    <div class='beer-price is-right'>
+                        $${beer.price}
+                    </div>
+                    <div class='beer-style'>
                         ${beer.style}
-                            <span class='beer-abv'>
-                                ${beer.abv}%
-                            </span>
-                        <h5 class='beer-desc'>
-                                ${beer.description}
-                        </h5>
-                            <span class='beer-allergies'>
-                                ${beer.allergies}
-                            </span>
-                            <span class='beer-fav'>
-                                ${beer.favorite}
-                            </span>
-                            <span class='beer-bottle'>
-                                ${beer.bottle}
-                            </span>
-                            <span class='beer-draught'>
-                                ${beer.draught}
-                            </span>
-                    </h4>
+                    </div>
+                    <div class='beer-abv'>
+                        ${beer.abv}%
+                    </div>
+                    <div class='beer-desc'>
+                        ${beer.description}
+                    </div>
+                    <div class='beerAttr'>
+                        <div class='beerAllergy'><img src='./images/allergy-warning.png' class='icon'></div>
+                        <div class='beerFavorites'><img src='./images/favorites.png' class='icon'></div>
+                        <div class='bottle>'<img src='./images/spicy.png' class='icon'></div>
+                        <div class='draught'><img src='./images/vegan.png' class='icon'></div>
+                    </div>
+                </div>
         `);
     });
     //pulls ENTREE information from MENU api
     data.entrees.forEach(function(entrees){
         $('.food').append(`
-                    <h3 class='entree-name level-left'>
-                    ${entrees.item}
-                        <span class='entree-price level-right'>
-                            $${entrees.price}
-                        </span>
-                    </h3>
-                    <h5 class='entree-desc'>
+                    <div class='entree level-left'>
+                        <div class='entree-name'>
+                            ${entrees.item}
+                        </div>
+                        <div class='entree-price level-right'>
+                            ${entrees.price}
+                        </div>
+                        <div class='entree-desc'>
                             ${entrees.description}
-                    </h5>
-                        <span class='entree-allergies'>
-                            ${entrees.allergies}
-                        </span>
-                        <span class='entree-fav'>
-                            ${entrees.favorite}
-                        </span>
-                        <span class='entree-spicy'>
-                            ${entrees.spicy}
-                        </span>
-                        <span class='entree-vegan'>
-                            ${entrees.vegan}
-                        </span>
+                        </div>
+                        <div class='entree-attr'>
+                            <div class='allergy'><img src='./images/allergy-warning.png' class='icon'></div>
+                            <div class='favorites'><img src='./images/favorites.png' class='icon'></div>
+                            <div class='spicy>'<img src='./images/spicy.png' class='icon'></div>
+                            <div class='vegan'><img src='./images/vegan.png' class='icon'></div>
+                        </div>
+                    </div>
         `);
     });
     //pulls GAMES information from MENU api
     data.games.forEach(function(games){
         $('.play').append(`
-                <h3 class='game-name level-left'>
+                <div class='game level-left'>
+                    <div class='game-name'>
                         ${games.item}
-                    <span class='game-price level-right'>
-                        $${games.price}
-                    </span>
-                </h3>
-                    <h5 class='game-desc'>
-                            ${games.description}
-                    </h5>
-                        <span class='game-fav'>
-                            ${games.favorite}
-                        </span>
-                        <span class='game-online'>
-                            ${games.online}
-                        </span>
-                        <span class='game-multi'>
-                            ${games.multiplayer}
-                        </span>
-                        <span class='game-rating'>
-                            ${games.rating}
-                        </span>
-                        <span class='game-platform>
-                            ${games.platform}
-                        </span>
+                    </div>
+                    <div class='game-price level-right'>
+                        ${games.price}
+                    </div>
+                    <div class='game-desc'>
+                        ${games.description}
+                    </div>
+                    <div class='game-attr'>
+                        <div class='online'><img src='./images/allergy-warning.png' class='icon'></div>
+                        <div class='multiplayer'><img src='./images/favorites.png' class='icon'></div>
+                        <div class='rating>'<img src='./images/spicy.png' class='icon'></div>
+                        <div class='favorite'><img src='./images/vegan.png' class='icon'></div>
+                    </div>
+                </div>
         `);
     });
 }
